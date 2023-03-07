197d71bc34cd2cefe6018f731f2fb00fd8fbda39
MFellen <m_fellen@live.concordia.ca>
2023-03-06 21:40:50 -0500

    Priority, sjf and FS

--- src/FairShare.java
+++ src/FairShare.java
@@ -9,17 +9,22 @@ public class FairShare implements Algorithm{
     private static int arrivalTime = 0;
     int quantum = 30;
     int userQuantum;
+    int totalBurst = 0;
+    int numberTasks;
     List<Task> userTasks = new ArrayList<>();
     int user  = 0;
     public FairShare(List<Task> queue) {
         tasks = queue;
+        for (Task task : tasks){
+            totalBurst += task.getBurst();
+        }
+        numberTasks = tasks.size();
     }
     @Override
     public void schedule() {
         int currentTime = arrivalTime;
         int totalWait = 0;
         int totalTurn= 0;
-        int numberTasks = tasks.size();
         do
         {
             Task task = pickNextTask();
@@ -29,23 +34,25 @@ public class FairShare implements Algorithm{
             if (task.getBurst() <= userQuantum){
                 System.out.println(task.getName() + " Finished");
                 currentTime += task.getBurst();
+                int turn = currentTime;
+                totalTurn += turn;int wait = currentTime;
+                totalWait += wait;
             }
             else {
                 tasks.add(task);
                 currentTime += userQuantum;
                 task.setBurst(task.getBurst()-userQuantum);
             }
-//            int wait = currentTime;
-//            totalWait += wait;
-//            int turn = currentTime;
-//            totalTurn += turn;
+            int wait = currentTime;
+            totalWait += wait;
+
 //            System.out.println("Wait: " + wait + " Turn: " + turn + "\n");
         } while (!tasks.isEmpty());
         completionTime = currentTime;
         System.out.println("Completion Time: " + completionTime);
-//        avgWaiting = totalWait / numberTasks;
-//        avgTurnAround = totalTurn / numberTasks;
-//        System.out.println("avgTurn: " + avgTurnAround+ " avgWait: " + avgWaiting);
+        avgWaiting = (totalWait - totalBurst) / numberTasks;
+        avgTurnAround = totalTurn / numberTasks;
+        System.out.println("avgTurn: " + avgTurnAround+ " avgWait: " + avgWaiting);
     }
 
     @Override
